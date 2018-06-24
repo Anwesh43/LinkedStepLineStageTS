0f8f893e0be941aa9b6effd5461cc6da556b20eb
Anwesh43 <anweshthecool0@gmail.com>
2018-06-25 01:30:25 +0530

    adding spawn and executing js in webdriver to correct automation logic

--- gulpFile.js
+++ gulpFile.js
@@ -1,23 +1,46 @@
 const gulp = require('gulp')
 const {Builder, By} = require('selenium-webdriver')
-const cp = require('cp')
+const cp = require('child_process')
 async function openPage() {
     let driver = await new Builder().forBrowser('chrome').build()
     try {
       await driver.get('http://localhost:8000')
-      for (var i = 0; i < 10; i++) {
-          await driver.findElement(By.tagName('canvas').click())
-          await driver.wait(() => false, 500)
+      for (var i = 0; i < 20; i++) {
+          await driver.findElement(By.js(() => {
+
+              return document.getElementsByTagName('canvas')[0]
+          })).click()
+          await driver.sleep(1000)
       }
+      await driver.sleep(2000)
     }  finally {
         await driver.quit()
     }
 }
 gulp.task('default', () => {
-    cp.exec('python -m SimpleHTTPServer',(err, stdout, stderr) => {
-        if (err == null) {
-            console.log(stdout)
-            await openPage()
-        }
+    console.log('starting the server')
+    const startServerProcess = cp.spawn('python', ['-m', 'SimpleHTTPServer'], {stdio:'pipe'})
+
+    startServerProcess.stdout.on('data', (data) => {
+        console.log(data)
     })
+
+    startServerProcess.stderr.on('data', (data) => {
+        console.log(data.toString())
+    })
+
+    startServerProcess.on('error', () => {
+        console.log("error starting the server")
+    })
+
+    setTimeout(() => {
+        console.log("server connected on port 8000")
+        async function start() {
+            await openPage()
+            startServerProcess.kill()
+        }
+        start()
+    }, 1000)
+
+
 })
